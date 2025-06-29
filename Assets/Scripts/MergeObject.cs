1b952bc0ee2aad8a0c4075d92d984f6f7274a085
FeliGW <alloisfelipe9@gmail.com>
2025-06-28 21:19:13 -0300

    Sistema Puntos

--- Assets/Scripts/MergeObject.cs
+++ Assets/Scripts/MergeObject.cs
@@ -8,10 +8,12 @@ public class MergeObject : MonoBehaviour
     public int level;
     private bool hasMerged = false;
     private MergeManager mergeManager;
+    private puntitos scoreManager;
 
     void Start()
     {
         mergeManager = FindObjectOfType<MergeManager>();
+        scoreManager = FindObjectOfType<puntitos>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -35,20 +37,24 @@ public class MergeObject : MonoBehaviour
 
         Vector3 spawnPosition = (transform.position + other.transform.position) / 2f;
 
+        if (scoreManager != null)
+        {
+            float puntos = 10 * (level + 1); // nivel del objeto resultante
+            scoreManager.AgregarPuntos(puntos);
+        }
+
         if (level >= mergeManager.prefabs.Length - 1)
         {
-            // Máximo tier -> +1 token
             CurrencyManager.Instance.AddToken(1);
             Explode();
             Destroy(other.gameObject);
         }
         else
         {
-            mergeManager.SpawnMerged(level + 1, spawnPosition);
+           mergeManager.SpawnMerged(level + 1, spawnPosition);
             Destroy(other.gameObject);
         }
-
-        Destroy(gameObject);
+     Destroy(gameObject);
     }
 
     private void Explode()
